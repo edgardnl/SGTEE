eb79277e3f952ce1ad19af2823650b7a864a3be2
edgardnl <40221456+edgardnl@users.noreply.github.com>
2018-06-19 00:09:37 -0500

    services

---
+++ services/ObjetoServices.php
@@ -0,0 +1,11 @@
+<?php
+
+class ObjetoServices
+{
+	
+	var $id_seguimiento;
+	var $id_alumno;
+	var $nombre;
+	var $ap_p;
+	var $ap_m;
+}
--- services/SeguimientoTutorialAlumnos.php
+++ services/SeguimientoTutorialAlumnos.php
@@ -1,4 +1,5 @@
-<?php
+<?php	
+
 	$con = mysqli_connect("localhost","root","","sgtee");
 	if ($con->connect_errno) {
 		die("Fallo la conexion");
@@ -6,20 +7,22 @@
 		//echo "Conexion existosa \n";
 	}
 
-	$idtutor = $_POST['idtu'];
+	$tutor = 1;//$_POST['tutor'];
 
-	
-	$query = "SELECT seguimiento_tutor.id_seguimiento,relacion_alumno_tutor.id_alumno,alumno.nombre,alumno.ap_p,alumno.ap_m 
+	//La consulta trae a is alumnos que tiene el tutor asignado en tutorias especiales los campos son (id_seguimineto,id_alumno,nombre,ap_p,ap_m)	
+
+    $query = "SELECT seguimiento_tutor.id_seguimiento,relacion_alumno_tutor.id_alumno,alumno.nombre,alumno.ap_p,alumno.ap_m 
 			FROM seguimiento_tutor,relacion_alumno_tutor,alumno,tutor
 			WHERE seguimiento_tutor.id_relacion = relacion_alumno_tutor.id_relacion
 			AND relacion_alumno_tutor.id_alumno = alumno.id_alumno
 			AND relacion_alumno_tutor.id_tutor = tutor.id_tutores
-			AND tutor.id_tutores = $idtu";
+			AND tutor.id_tutores = $tutor";
 
-	$query1 = "SELECT COUNT(actividades.id_actividades) as n_actividades
+	//Esta consulta trae el numero de actividades por alumno lo cual las busca atravez del id_seguimiento de la tabla seguimineto tutorias
+	/*$query1 = "SELECT COUNT(actividades.id_actividades) as n_actividades
             FROM actividades,seguimiento_tutor
             WHERE actividades.id_seguimiento = seguimiento_tutor.id_seguimiento
-            AND seguimiento_tutor.id_seguimiento = $idseg";
+            AND seguimiento_tutor.id_seguimiento = $id_seguimiento";*/
 	
 	
 	$result = mysqli_query($con,$query);
